512c95aaaab98002e251b309bbd4da581edbac7d
Yurii Kukharskyi <yurii.kukharskyi@gmail.com>
2020-05-28 23:48:12 +0300

    Improved navigation

--- src/main/java/com/kpi/fileaccess/controllers/UserController.java
+++ src/main/java/com/kpi/fileaccess/controllers/UserController.java
@@ -8,44 +8,46 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
     private UserService userService;
 
-    @GetMapping("/users")
+    @GetMapping
     public String renderUsersList(Model model) {
         model.addAttribute("users", userService.readAll());
         return "users_list";
     }
 
-    @GetMapping("/user")
+    @GetMapping("/create")
     public String renderNewUserForm(Model model) {
         model.addAttribute("user", new User());
         return "user_form";
     }
 
-    @GetMapping("/user/{id}")
+    @GetMapping("/edit/{id}")
     public String renderEditUserForm(@PathVariable Long id, Model model) {
         model.addAttribute("user", userService.read(id));
         return "user_form";
     }
 
-    @GetMapping("/user-delete/{id}")
+    @GetMapping("/delete/{id}")
     public String deleteUser(@PathVariable Long id) {
         userService.delete(id);
         return "redirect:/users";
     }
 
-    @PostMapping("/user-create")
+    @PostMapping("/create")
     public String createUser(User user) {
         userService.create(user);
         return "redirect:/users";
     }
 
-    @PostMapping("/user-edit/{id}")
+    @PostMapping("/edit/{id}")
     public String createUser(@PathVariable Long id, User user) {
         userService.edit(id, user);
         return "redirect:/users";
