1860b1b612b498db1137fc3496d311e487d29cd2
Yurii Kukharskyi <yurii.kukharskyi@gmail.com>
2020-05-28 18:36:03 +0300

    initial commit

---
+++ src/main/java/com/kpi/fileaccess/FileAccessApplication.java
@@ -0,0 +1,13 @@
+package com.kpi.fileaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kpi/fileaccess/FileAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpi.fileaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
